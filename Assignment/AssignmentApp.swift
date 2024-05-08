0680938f598ab3ba6162aad5720dab57128bd8a7
cam <camsemailacc@gmail.com>
2024-05-08 14:28:35 +1000

    Initial Commit

---
+++ Assignment/AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentApp.swift
+//  Assignment
+//
+//  Created by Cameron on 8/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
